02626a583aea428f202f88b66046ea8c7bfd36f9
Prakash <sivaprakashbandi07@gmail.com>
2025-10-08 21:17:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{textAlign:'center', marginTop:'100px'}}>
-      <h1>🚀 Wexa-AI</h1>
+      <h1> Wexa-AI</h1>
       <p>Next.js App deployed via Docker, GitHub Actions, and Minikube</p>
     </div>
   );
